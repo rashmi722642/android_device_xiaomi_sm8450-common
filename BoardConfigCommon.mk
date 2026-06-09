429d7d5e8be49b2a859ca7c1b0fd73387f65ac31
pabloescobar-reborn <yogivangala07@gmail.com>
2026-06-09 13:30:05 +0000

    sm8450-common: Include vendor/infinity.config to kernel configs target
    Change-Id: Iae491f996fe64a461ea1039ebdbf411d2f18c6bf
    Signed-off-by: pabloescobar-reborn <yogivangala07@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -82,7 +82,8 @@ TARGET_KERNEL_CONFIG := \
     vendor/waipio_GKI.config \
     vendor/xiaomi_GKI.config \
     vendor/$(PRODUCT_DEVICE)_GKI.config \
-    vendor/debugfs.config
+    vendor/debugfs.config \
+    vendor/infinity.config
 
 BOARD_BOOT_HEADER_VERSION := 4
 BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOT_HEADER_VERSION)
